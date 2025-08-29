605bb9c58642623a954441f8a065f0bb4f5cd246
DJ Dam's <162346842+djdams1@users.noreply.github.com>
2025-08-29 11:00:01 +0200

    Snail sans OO

---
+++ personnel/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.CursorVisible = false;
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+
+            for (i = 0; i < 21; i++) {
+                Console.SetCursorPosition(i, 0);
+                snail();
+
+                Console.SetCursorPosition(i, 0);
+                Console.Write("  ");
+                Thread.Sleep(250);
+            }
+            Console.Write(" _____");
+            Console.Read();
+        }
+        static void snail()
+        {
+            Console.Write("   __@_ö");
+        }
+    }
+}
